e2f95a28f8375a9b46982a9129adb561e016a3ec
Drew & Paul <student@epicodus.com>
2015-04-22 10:34:18 -0700

    adds fully passing test spec

--- title_case/lib/title_case.rb
+++ title_case/lib/title_case.rb
@@ -2,10 +2,16 @@ class String
   define_method(:title_case) do
     sentance = self.split(" ")
     uppercase_sentance = []
+    special_words = ["and"]
 
     sentance.each do |word|
-      word.capitalize!()
-      uppercase_sentance.push(word)
+      if special_words.include?(word)
+        uppercase_sentance.push(word)
+      else
+        word.capitalize!()
+        uppercase_sentance.push(word)
+      end
+
     end
 
     uppercase_sentance.join(" ")
