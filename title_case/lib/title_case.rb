0d15bb013c7a02bc6d794271f55510c527d557f1
Drew & Paul <student@epicodus.com>
2015-04-22 10:27:02 -0700

    adds nearly-passing upcase method

--- title_case/lib/title_case.rb
+++ title_case/lib/title_case.rb
@@ -1,5 +1,13 @@
 class String
   define_method(:title_case) do
-    self.capitalize!()
+    sentance = self.split(" ")
+    uppercase_sentance = []
+
+    sentance.each do |word|
+      word.capitalize!()
+      uppercase_sentance.push(word)
+    end
+
+    uppercase_sentance.join(" ")
   end
 end
