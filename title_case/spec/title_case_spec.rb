80c6e90bd390a79e50885e334ba32d63952cedbb
Drew & Paul <student@epicodus.com>
2015-04-22 10:49:13 -0700

    added rspec unit test for exclusion of special words

--- title_case/spec/title_case_spec.rb
+++ title_case/spec/title_case_spec.rb
@@ -9,4 +9,8 @@ describe("String#title_case") do
   it("capitalize the first letter of a multi-word title") do
     expect(("my egg").title_case()).to(eq("My Egg"))
   end
+
+  it("capitalize but exclude special words") do
+    expect(("my egg and i").title_case()).to(eq("My Egg and I"))
+  end
 end
